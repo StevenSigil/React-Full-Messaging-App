43bd9c2075e0b64446f10cc3a5448c0eba98d970
human1123 <selliott522@gmail.com>
2021-03-30 20:59:03 -0600

    Adjusted settings/env for deploy.

--- src/App.js
+++ src/App.js
@@ -18,7 +18,17 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 
-firebase.initializeApp();
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+}
+
+firebase.initializeApp(firebaseConfig);
+firebase.firestore();
 
 export default function App() {
   const [user, loading, error] = useAuthState(firebase.auth());
